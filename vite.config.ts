972188011ff2b1aae556f76f66a6cfb6c8a127d9
Navanath3137 <navanath@navanath.navanath>
2026-04-21 12:51:54 +0530

    Fresh deployment: synchronized local files and fixed Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Dr-M-Venkat-Ratnam/',
+  base: '/Dr.M.Venkat_Ratnam/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
